9e5350013251d764c420fe43ebd846cd63b1a91f
Michael Clausen <encodeering@gmail.com>
2016-05-22 16:46:14 +0200

    openjdk should be build if the custom flag holds the correct value
    * wraps the previous build-code in a case statement

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,14 +11,20 @@ case "$VERSION" in
     *    ) exit 1
 esac
 
-docker pull   "$REPOSITORY/buildpack-$ARCH:$PACKTAG"
-docker tag -f "$REPOSITORY/buildpack-$ARCH:$PACKTAG" "buildpack-deps:$PACKTAG"
+case "$CUSTOM" in
+    openjdk )
+        docker pull   "$REPOSITORY/buildpack-$ARCH:$PACKTAG"
+        docker tag -f "$REPOSITORY/buildpack-$ARCH:$PACKTAG" "buildpack-deps:$PACKTAG"
 
-patch -p1 --no-backup-if-mismatch --directory=$PROJECT < .patch/$VERSION/Dockerfile.patch
+        patch -p1 --no-backup-if-mismatch --directory=$PROJECT < .patch/$VERSION/Dockerfile.patch
 
-docker build -t "$TAG:$TAGSPECIFIER" \
-             --build-arg ARCH="$ARCH" \
-             --build-arg JAVA_DEBIAN_VERSION="$VERSIONPIN" \
-             "$PROJECT/$VERSION"
+        docker build -t "$TAG:$TAGSPECIFIER" \
+                     --build-arg ARCH="$ARCH" \
+                     --build-arg JAVA_DEBIAN_VERSION="$VERSIONPIN" \
+                     "$PROJECT/$VERSION"
 
-docker run --rm "$TAG:$TAGSPECIFIER" java -version
+        docker run --rm "$TAG:$TAGSPECIFIER" java -version
+        ;;
+esac
+
+
