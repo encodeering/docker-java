9c3b42e51f13fad91bbaec7c3670c85017212436
Michael Clausen <encodeering@gmail.com>
2018-01-02 16:12:13 +0100

    pins version to a specific commit
    * latest commit that uses jessie as base image
    
    NOTE
    
    * patch needs to contain the latest debian pins as they dont exist anymore
    ** v8 hasn't been updated at all

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,17 +11,14 @@ case "$VERSION" in
     *    ) exit 1
 esac
 
-docker pull   "$REPOSITORY/buildpack-$ARCH:$PACKTAG"
-docker tag -f "$REPOSITORY/buildpack-$ARCH:$PACKTAG" "buildpack-deps:$PACKTAG"
+docker pull "$REPOSITORY/buildpack-$ARCH:$PACKTAG"
+docker tag  "$REPOSITORY/buildpack-$ARCH:$PACKTAG" "buildpack-deps:$PACKTAG"
 
 case "$CUSTOM" in
     openjdk )
         patch -p1 --no-backup-if-mismatch --directory=$PROJECT < .patch/$VERSION/Dockerfile.patch
 
-        docker build -t "$TAG:$TAGSPECIFIER" \
-                     --build-arg ARCH="$ARCH" \
-                     --build-arg JAVA_DEBIAN_VERSION="$VERSIONPIN" \
-                     "$PROJECT/$VERSION"
+        docker build -t "$TAG:$TAGSPECIFIER" "$PROJECT/$VERSION"
 
         docker run --rm "$TAG:$TAGSPECIFIER" java -version
         ;;
