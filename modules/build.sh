8cb203dfe3608d1d9479e623a95b08dd3ed1f8b8
Michael Clausen <encodeering@gmail.com>
2018-09-15 17:39:44 +0200

    removes oracle from build matrix
    * download requires an account

--- modules/build.sh
+++ modules/build.sh
@@ -6,16 +6,3 @@ import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
 ./build-${BASE}.sh
-
-case "$VARIANT" in
-    openjdk )
-        docker-verify java -version
-        ;;
-    oracle )
-           docker-verify-config "-e eula-java=accept"
-        if docker-verify java -version; then             true; fi
-
-           docker-verify-config "-e eula-java="
-        if docker-verify java -version; then false; else true; fi
-        ;;
-esac
