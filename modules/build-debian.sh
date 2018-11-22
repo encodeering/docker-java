dab45de20d70d337b21d359d7464599521ae9234
Michael Clausen <encodeering@gmail.com>
2018-11-22 18:59:43 +0100

    updating scm repository to latest revision
    * 5a23ec5ab11beacb71f89ec9f9935c52ab7e44bb

--- modules/build-debian.sh
+++ modules/build-debian.sh
@@ -6,8 +6,10 @@ import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
 case "$VERSION" in
-    *-jre) PACKTAG=jessie-curl ;;
-    *-jdk) PACKTAG=jessie-scm  ;;
+    7-jre) PACKTAG=jessie-curl ;;
+    7-jdk) PACKTAG=jessie-scm  ;;
+    8-jre) PACKTAG=stretch-curl ;;
+    8-jdk) PACKTAG=stretch-scm  ;;
     *    ) exit 1
 esac
 
