3255c917f75eba4c2d455bc19dd4b4ba7e6d28e1
Michael Clausen <encodeering@gmail.com>
2019-06-01 11:09:13 +0200

    removes alpine build scripts
    * unused, can be reimplemented later again
    
    NOTE
    
    * artifact from 8cb203dfe3608d1d9479e623a95b08dd3ed1f8b8

--- modules/build.sh
+++ modules/build.sh
@@ -5,4 +5,14 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-./build-${BASE}.sh
+case "$VERSION" in
+    8-jre) PACKTAG=stretch-curl ;;
+    8-jdk) PACKTAG=stretch-scm  ;;
+    *    ) exit 1
+esac
+
+docker-pull "$REPOSITORY/buildpack-$ARCH:$PACKTAG" "buildpack-deps:$PACKTAG"
+
+docker-build "$PROJECT/${VERSION%%-*}/${VERSION##*-}"
+
+docker-verify java -version 2>&1 | dup | contains "${ALIAS}"
